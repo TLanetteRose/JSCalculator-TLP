58fffa176b322a237befa1c75f80e55e002ef121
T. Lanette <TLPollard4516@gmail.com>
2020-04-26 15:56:15 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- jscalculator-tlp/src/layout/Calculator/Calculator.js
+++ jscalculator-tlp/src/layout/Calculator/Calculator.js
@@ -28,6 +28,60 @@ class Calculator extends React.Component {
         this.handleNumbers = this.handleNumbers.bind(this);
     }
 
+    maxDigitWarning() {
+        this.setState({
+            currentVal: "Too many digits",
+            prevVal: this.state.currentVal
+        });
+        setTimeout(() => this.setState({
+             currentVal: this.state.prevVal
+        }), 1000);
+    }
+
+    handleEvaluate() {
+        if (!this.state.currentVal.includes("Limit")) {
+            let expression = this.state.formula;
+            while (endsWithOperator.test(expression)) {
+                expression = expression.slice(0, -1);
+            }
+            expression = expression.replace(/x/g, "*").replace(/-/g, "-");
+            // eslint-disable-next-line 
+            let answer = Math.round(1000000000000 * eval(expression)) / 1000000000000;
+            this.setState({
+                currentVal: answer.toString(),
+                formula: expression.replace(/\*/g, ".").replace(/-/g, "-") + "=" + answer,
+                prevVal: answer,
+                evaluated: true
+            });
+        }
+    }
+
+    handleOperators(e) {
+        if (!this.state.currentVal.includes("Limit")) {
+            const value = e.target.value;
+            const { formula, prevVal, evaluated } = this.state;
+            this.setState({ currentVal: value, evaluated: false });
+            if (evaluated) {
+                this.setState({ formula: prevVal + value });
+            } else if (!endsWithOperator.test(formula)) {
+                this.setState({
+                    prevVal: formula,
+                    formula: formula + value
+                });
+            } else if (!endsWithNegativeSign.test(formula)) {
+                this.setState({
+                   formula: (endsWithNegativeSign.test(formula + value) ? formula : prevVal) + value
+                });
+            } else if (value !== "-") {
+                this.setState({
+                    formula: prevVal + value
+                });
+            }
+        }
+    }
+
+    
+
     initialize() {
         this.setState({
             currentVal: "0",
