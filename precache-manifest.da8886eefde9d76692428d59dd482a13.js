18e89421107e6a7813ec4f790709b3cd6133204f
T. Lanette <TLPollard4516@gmail.com>
2020-05-02 16:47:57 -0400

    Updates

---
+++ precache-manifest.da8886eefde9d76692428d59dd482a13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b79e8128eb58101e55b4e94039216ab7",
+    "url": "/JSCalculator-TLP/index.html"
+  },
+  {
+    "revision": "795972f843ff4b02ba3f",
+    "url": "/JSCalculator-TLP/static/css/main.8a64a96f.chunk.css"
+  },
+  {
+    "revision": "dbf697faa61ee6789c08",
+    "url": "/JSCalculator-TLP/static/js/2.e1435981.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JSCalculator-TLP/static/js/2.e1435981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "795972f843ff4b02ba3f",
+    "url": "/JSCalculator-TLP/static/js/main.9b2e0212.chunk.js"
+  },
+  {
+    "revision": "fed339ecbb4edf2cb8b8",
+    "url": "/JSCalculator-TLP/static/js/runtime-main.b5ccd726.js"
+  }
+]);
